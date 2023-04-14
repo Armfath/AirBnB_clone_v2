4ea5421cc4b9f5df8776fffcb30d7286eec375ea
Armand ANAGONOU <info.armanagonou@gmail.com>
2023-04-14 16:31:05 +0000

    continuing depoyment setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
 
+command -v nginx &> /dev/null || sudo apt update &> /dev/null && sudo apt install -y nginx &> /dev/null
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
@@ -10,7 +11,26 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
 
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
 
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        autoindex off;
+    }
+}" > /etc/nginx/sites-available/hbnb_static
+ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+service nginx restart
