fd47cf6c9d7c270802a7a9aa0bcb91cf58116a5f
Armand ANAGONOU <info.armanagonou@gmail.com>
2023-04-15 16:47:13 +0000

    fix pycode errrors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,12 +7,12 @@ from fabric.api import *
 import datetime
 
 def do_pack():
-	now = datetime.datetime.now()
-	archive = './version/web_static_{}{}{}{}{}{}.tgz'.format(now.year,
-	    now.month, now.day, now.hour, now.minute, now.second)
-	to_archive = './web_static'
-	try:
-		local('mkdir -p version && tar -czvf {} {}'.format(archive, to_archive))
-		return (archive)
-	except:
-		return None
+    now = datetime.datetime.now()
+    archive = './version/web_static_{}{}{}{}{}{}.tgz'.format(now.year,
+        now.month, now.day, now.hour, now.minute, now.second)
+    to_archive = './web_static'
+    try:
+        local('mkdir -p version && tar -czvf {} {}'.format(archive, to_archive))
+        return (archive)
+    except:
+        return None
