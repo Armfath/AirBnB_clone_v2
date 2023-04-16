cfd18374ab1971702138df32eb46432d3a9ffa18
ANAGONOU Armand <info.armanagonou.com>
2023-04-16 10:59:54 +0100

    test pycoodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,17 +2,19 @@
 """ Fabric to deploy an achieve for hbnb_web_static and deploy to eb servers
 """
 from fabric.api import local, env, put, run, settings
+from datetime import datetime
+
+
+env.hosts = ['100.25.211.4',
+             '100.26.234.235']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
 
 
 def do_pack():
     """ Archive web_static
     """
-    import datetime
-    env.hosts = ['100.25.211.4',
-             '100.26.234.235']
-    env.user = 'ubuntu'
-    env.key_filename = '~/.ssh/school'
-    now = datetime.datetime.now()
+    now = datetime.now()
     archive = './versions/web_static_{}{}{}{}{}{}.tgz\
     '.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
     to_archive = './web_static'
@@ -22,3 +24,9 @@ def do_pack():
         return (archive)
     else:
         return None
+
+
+def do_deploy(archive_path):
+    """ Distributes an archive to your web servers
+    """
+    pass
