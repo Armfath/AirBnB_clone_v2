d01d58583ef40b885c7e25c3a02811ef0f072dc9
ANAGONOU Armand <info.armanagonou.com>
2023-04-16 10:57:26 +0100

    test pycoodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,17 +2,13 @@
 """ Fabric to create an achieve for hbnb_web_static
 """
 from fabric.api import *
+from datetime import datetime
 
 
 def do_pack():
     """ Archive web_static
     """
-    import datetime
-    env.hosts = ['100.25.211.4',
-             '100.26.234.235']
-    env.user = 'ubuntu'
-    env.key_filename = '~/.ssh/school'
-    now = datetime.datetime.now()
+    now = datetime.now()
     archive = './versions/web_static_{}{}{}{}{}{}.tgz\
     '.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
     to_archive = './web_static'
