0b1786be020f28df4c124f112ea8f5a6e14c6b30
ANAGONOU Armand <info.armanagonou.com>
2023-04-16 11:38:01 +0100

    revert

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,8 +2,6 @@
 """ Fabric to deploy an achieve for hbnb_web_static and deploy to eb servers
 """
 from fabric.api import local, env, put, run, settings
-from datetime import datetime
-import os
 
 
 env.hosts = ['100.25.211.4',
@@ -15,7 +13,8 @@ env.key_filename = '~/.ssh/school'
 def do_pack():
     """ Archive web_static
     """
-    now = datetime.now()
+    import datetime
+    now = datetime.datetime.now()
     archive = './versions/web_static_{}{}{}{}{}{}.tgz\
     '.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
     to_archive = './web_static'
@@ -30,6 +29,7 @@ def do_pack():
 def do_deploy(archive_path):
     """ Distributes an archive to your web servers
     """
+    import os
     if os.path.exists(archive_path):
         archive_name_e = os.path.basename(archive_path)
         archive_name = archive_name_e.split('.')[0]
@@ -37,6 +37,27 @@ def do_deploy(archive_path):
         return False
 
     root = '/data/web_static'
+    with settings(warn_only=True):
+        opp_1 = put(f'{archive_path}', f'/tmp/{archive_name_e}')
+        opp_2 = run(f'mkdir -p {root}/releases/{archive_name}/')
+        opp_3 = run(f'tar -xzf /tmp/{archive_name_e}\
+                    -C {root}/releases/{archive_name}/')
+        opp_4 = run(f'rm /tmp/{archive_name_e}')
+        opp_5 = run(f'mv {root}/releases/{archive_name}/web_static/*\
+                    {root}/releases/{archive_name}/')
+        opp_6 = run(f'rm -rf {root}/releases/{archive_name}/web_static')
+        opp_7 = run(f'rm -rf {root}/current')
+        opp_8 = run(f'ln -s {root}/releases/{archive_name}/ {root}/current')
+
+        if (opp_1.failed or
+                opp_2.failed or
+                opp_3.failed or
+                opp_4.failed or
+                opp_5.failed or
+                opp_6.failed or
+                opp_7.failed or
+                opp_8.failed):
+            return False
     
     print("New version deployed!")
     return True
